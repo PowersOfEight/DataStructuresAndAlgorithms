a02dfb5fb50db0de65f336c68d0f66795ff6f049
James Daniel Johnson <jdjohnson8883@gmail.com>
2023-01-02 20:08:08 -0500

    Added TreeNode.py with setLeft and setRight

---
+++ python/TreeNode.py
@@ -0,0 +1,13 @@
+from Node import Node
+
+class TreeNode(Node):
+    left: 'TreeNode'
+    right: 'TreeNode'
+    
+    def setLeft(self, left: 'TreeNode'):
+        self.left = left
+        
+    def setRight(self, right: 'TreeNode'):
+        self.right = right
+        
+        
