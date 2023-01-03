113d4e0cfe7dc881187eee7b83b04c235afd8038
James Daniel Johnson <jdjohnson8883@gmail.com>
2023-01-03 12:18:56 -0500

    Added recursive destructor to TreeNode

--- C++/Tree/TreeNode.h
+++ C++/Tree/TreeNode.h
@@ -1,6 +1,9 @@
 #ifndef TREENODE_H
 #define TREENODE_H
 #include "../Node/Node.h"
+#include <iostream>
+using namespace std;
+
 template<typename T> class TreeNode : 
     public Node<T> {
         TreeNode<T>* m_left;
@@ -8,7 +11,16 @@ template<typename T> class TreeNode :
 public:
     TreeNode(T val) : 
         Node<T>(val), m_left(nullptr), m_right(nullptr) {}
-
+    ~TreeNode() {
+        if (m_left){
+            m_left->~TreeNode();
+            delete m_left;
+        } 
+        if (m_right){
+            m_right->~TreeNode();
+            delete m_right;
+        }
+    }
     void setLeft(TreeNode<T>* left) { this->m_left = left; }
     void setRight(TreeNode<T>* right) { this->m_right = right; }
     TreeNode<T>* left() const { return m_left; }
